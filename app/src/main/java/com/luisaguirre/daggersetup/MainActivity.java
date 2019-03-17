eedddd0aaff456db7a4b9df654b60118ff534b22
Luis Aguirre <kourosvi@gmail.com>
2019-03-16 23:07:44 -0400

    Add android app without DI

--- app/src/main/java/com/luisaguirre/daggersetup/MainActivity.java
+++ app/src/main/java/com/luisaguirre/daggersetup/MainActivity.java
@@ -1,13 +1,100 @@
 package com.luisaguirre.daggersetup;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.luisaguirre.daggersetup.model.RandomUsers;
+
+import java.io.File;
+
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+    Retrofit retrofit;
+    RecyclerView recyclerView;
+    RamdomUserAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        Timber.plant(new Timber.DebugTree());
+
+        File cacheFile = new File(this.getCacheDir(), "HttpCache");
+        cacheFile.mkdirs();
+
+        Cache cache = new Cache(cacheFile, 10 * 1000 * 1000); //10 MB
+
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Timber.i(message);
+            }
+        });
+
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient()
+                .newBuilder()
+                .cache(cache)
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://randomuser.me/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        populateUsers();
+    }
+
+    private void populateUsers() {
+        Call<RandomUsers> randomUsersCall = getRandomUserService().getRandomUsers(10);
+        randomUsersCall.enqueue(new Callback<RandomUsers>() {
+            @Override
+            public void onResponse(Call<RandomUsers> call, @NonNull Response<RandomUsers> response) {
+                if(response.isSuccessful()) {
+                    mAdapter = new RamdomUserAdapter();
+                    mAdapter.setItems(response.body().getResults());
+                    recyclerView.setAdapter(mAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RandomUsers> call, Throwable t) {
+                Timber.i(t.getMessage());
+            }
+        });
+    }
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public RandomUsersApi getRandomUserService() {
+        return retrofit.create(RandomUsersApi.class);
     }
 }
